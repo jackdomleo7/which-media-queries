6f58d3f842741f0379a8fc4f1c8622d2764279c6
Jack Domleo <jackdomleo7@gmail.com>
2025-03-06 00:29:14 +0000

    feat(footer): Add link to GitHub

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
   </header>
 
   <RouterView />
+
+  <footer>
+    <a href="https://github.com/jackdomleo7/which-media-queries">GitHub</a>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -13,5 +17,10 @@ import { RouterView } from 'vue-router'
 <style lang="scss" scoped>
 header {
   font-size: 2rem;
+}
+
+footer {
+  margin-top: 4rem;
+  text-align: center;
 }
 </style>
