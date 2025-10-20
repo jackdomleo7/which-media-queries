7aba8775e3ed49c94f0f9ca687da58ab8c505a2d
Jack Domleo <jackdomleo7@gmail.com>
2025-10-20 22:41:09 +0100

    feat: Add aspect-ratio, color, color-index, and resolution (with ranges)

--- src/data/list.ts
+++ src/data/list.ts
@@ -4,7 +4,10 @@ export const items: MediaQueryItemModel[] = [
   // Media queries that take specific keyword values only
   { query: 'any-pointer', values: ['none', 'coarse', 'fine'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/any-pointer' },
   { query: 'any-hover', values: ['none', 'hover'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/any-hover' },
+  { query: 'aspect-ratio', values: ['1/1', '4/3', '3/2', '16/10', '16/9', '21/9', '32/9'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/aspect-ratio' },
+  { query: 'color', values: ['0', '1', '8', '16', '24', '32', '48'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/color' },
   {  query: 'color-gamut', values: ['srgb', 'p3', 'rec2020'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/color-gamut' },
+  { query: 'color-index', values: ['0', '256', '32768', '65536', '16777216'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/color-index' },
   { query: 'display-mode', values: ['browser', 'fullscreen', 'minimal-ui', 'picture-in-picture', 'standalone', 'window-controls-overlay'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/display-mode' },
   { query: 'dynamic-range', values: ['standard', 'high'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/dynamic-range' },
   { query: 'forced-colors', values: ['none', 'active'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors' },
@@ -21,6 +24,7 @@ export const items: MediaQueryItemModel[] = [
   { query: 'prefers-reduced-data', values: ['no-preference', 'reduce'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-data' },
   { query: 'prefers-reduced-motion', values: ['no-preference', 'reduce'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion' },
   { query: 'prefers-reduced-transparency', values: ['no-preference', 'reduce'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-transparency' },
+  { query: 'resolution', values: ['72dpi', '96dpi', '120dpi', '144dpi', '192dpi', '240dpi', '288dpi', '384dpi'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/resolution' },
   { query: 'scan', values: ['progressive', 'interlace'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/scan' },
   { query: 'scripting', values: ['none', 'initial-only', 'enabled'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/scripting' },
   { query: 'update', values: ['none', 'slow', 'fast'], docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@media/update' },
